a8fa12fd9e6d4dc90c3ca8d52f6b4a1f58200e32
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-13 06:36:43 +0000

    feat: update menu

--- number_guess.sh
+++ number_guess.sh
@@ -13,32 +13,40 @@ then
   echo Username required. Please try again.
 else 
   # TODO validate input
-  USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
-
+  # USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played, MIN(guesses) AS best_game FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
+  USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
+  
   if [[ -z $USER_ID ]]
   then 
     echo "Welcome, $USERNAME! It looks like this is your first time here."
     # TODO: create new user with entered username
     INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-    echo $INSERT_USER_RESULT;
+    if [[ $INSERT_USER_RESULT != "1|0" ]]
+    then 
+      echo $INSERT_USER_RESULT
+    fi
 
-    USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
+    USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
+    echo $USER_ID
   else
     echo "$USER_ID"
+    # get games data
+    # "SELECT COUNT(game_id) AS games_playes, MIN(guesses) as best_game"
     echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
   fi
 
   GAME_FINISHED=false
   GUESSES=0
   echo "Guess the secret number between 1 and 1000:"
-  
+    
+  # Create game here:
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, 1)")
+
   while [[ "$GAME_FINISHED" != "true" ]] ; do
     read USER_GUESS
     GUESSES=$(( $GUESSES + 1 ))
-    # Create game here:
-    # "INSERT INTO games(user_id) "
-
-    echo "$USER_GUESS ? $SECRET_NUMBER : $GUESSES"
+    
+    echo "$USER_ID guessed $USER_GUESS. there have been $GUESSES guesses to reach $SECRET_NUMBER"
 
     if [ $USER_GUESS -eq $SECRET_NUMBER ]
     then
