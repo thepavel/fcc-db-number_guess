6e68cfac9a4a2b1f902982d738e0ed138461c1ee
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-13 06:13:19 +0000

    feat: adds testuser to users for testing

--- number_guess.sql
+++ number_guess.sql
@@ -136,6 +136,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.users VALUES (1, 'testuser');
 
 
 --
@@ -149,7 +150,7 @@ SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_user_id_seq', 1, true);
 
 
 --
