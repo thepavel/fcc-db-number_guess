e15377af1268e753e57c8cb0e923c914d9318e5f
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-13 05:59:58 +0000

    feat: display guess menu in loop
    fix: error output
    
    feat: adds testuser to users for testing
    
    feat: update menu
    
    feat: adds game and another test user
    
    feat: adds is_completed in case needed to distinguish
    
    fix: query to get games info
    
    feat: insert game

--- number_guess.sh
+++ number_guess.sh
@@ -13,31 +13,84 @@ then
   echo Username required. Please try again.
 else 
   # TODO validate input
-
-  USER_ID=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
-
+  # USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played, MIN(guesses) AS best_game FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
+  USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
+  
   if [[ -z $USER_ID ]]
   then 
-    # create new user with entered username
     echo "Welcome, $USERNAME! It looks like this is your first time here."
+    # TODO: create new user with entered username
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    if [[ $INSERT_USER_RESULT =~ "INSERT 0 1$" ]]
+    then 
+      echo $INSERT_USER_RESULT
+    fi
+    
+    USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
   else
-
-    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+    
+    # get games data
+    GAMES_RESULT=$($PSQL "SELECT COUNT(game_id) AS games_played, MIN(guesses) as best_game FROM games")
+    
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
+  
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, 1) RETURNING game_id" )
+  GAME_ID=$( echo $INSERT_GAME_RESULT | sed -E 's/^([0-9]+).*$/\1/' )
 
   GAME_FINISHED=false
+  GUESSES=0
   echo "Guess the secret number between 1 and 1000:"
-  
-  # todo: loop
+
+  while [[ "$GAME_FINISHED" != "true" ]] ; do
     read USER_GUESS
-    if [[ $USER_GUESS =~ ^[0-9]{1,4}$ ]] # should be 1-1000, no more than 4 digits needed
+    GUESSES=$(( $GUESSES + 1 ))
+    # updating existing game
+      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET guesses = guesses + 1 WHERE game_id = $GAME_ID" )
+
+    echo "$USER_ID guessed $USER_GUESS. there have been $GUESSES guesses to reach $SECRET_NUMBER"
+
+    if [ "$USER_GUESS" -eq "$SECRET_NUMBER" ]
     then
-      # check against random number and tell user if higher or lower
-      echo "$USER_GUESS ? $SECRET_NUMBER"
-      # game ends at some point, uncomment below and delete this line:
-      # GAME_FINISHED=true
-    else # not valid input
-      echo That is not an integer, guess again:
+      echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+      GAME_FINISHED=true
+      # todo update game as finished? may make stats meaningless without that.
+      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET is_completed = true WHERE game_id = $GAME_ID" )
+      
+    else 
+      
+      GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
+      if [ $GUESSED_NUMBER -lt $SECRET_NUMBER ]
+      then
+        echo "It's higher than that, guess again:"
+      else 
+        if [ $GUESSED_NUMBER -gt $SECRET_NUMBER ]
+        then
+          echo "It's lower than that, guess again:"
+        else 
+          echo "That is not an integer, guess again:"
+        fi
+      fi
+
     fi
-  
+  done
+
+    #   if [[ $USER_GUESS =~ ^[0-9]{1,4}$ ]] # should be 1-1000, no more than 4 digits needed
+    #   then
+    #     if [[ $USER_GUESS < $SECRET_NUMBER ]]
+    #     then 
+    #       echo "It's lower than that, guess again:"
+    #     else if [[ $USER_GUESS > $SECRET_NUMBER ]]; then
+    #       echo "It's higher than that, guess again:"
+    #     else if [[ $USER_GUESS == $SECRET_NUMBER ]]; then
+    #       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    #       GAME_FINISHED=true
+    #     else 
+    #       echo "Unexpected input, guess again:"
+    #     fi 
+    #   else
+    #     echo "That is not an integer, guess again:"
+    #   fi
+    # # done
 fi
--- number_guess.sql
+++ number_guess.sql
@@ -50,7 +50,9 @@ SET default_table_access_method = heap;
 CREATE TABLE public.games (
     game_id integer NOT NULL,
     user_id integer NOT NULL,
-    guesses integer DEFAULT 0 NOT NULL
+    guesses integer DEFAULT 0 NOT NULL,
+    created_on timestamp without time zone DEFAULT now(),
+    is_completed boolean DEFAULT false
 );
 
 
@@ -130,26 +132,29 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.games VALUES (1, 2, 1, '2024-09-13 06:34:40.066525', true);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.users VALUES (1, 'testuser');
+INSERT INTO public.users VALUES (2, 'pav');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
+SELECT pg_catalog.setval('public.games_game_id_seq', 1, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_user_id_seq', 2, true);
 
 
 --
