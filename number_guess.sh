1393ab88604f2dfb3eee912f46092029e42a75fa
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-13 05:59:58 +0000

    feat: display guess menu in loop

--- number_guess.sh
+++ number_guess.sh
@@ -13,31 +13,68 @@ then
   echo Username required. Please try again.
 else 
   # TODO validate input
-
-  USER_ID=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+  USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
 
   if [[ -z $USER_ID ]]
   then 
-    # create new user with entered username
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-  else
+    # TODO: create new user with entered username
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo $INSERT_USER_RESULT;
 
+    USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
+  else
+    echo "$USER_ID"
     echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
   fi
 
   GAME_FINISHED=false
+  GUESSES=0
   echo "Guess the secret number between 1 and 1000:"
   
-  # todo: loop
+  while [[ "$GAME_FINISHED" != "true" ]] ; do
     read USER_GUESS
-    if [[ $USER_GUESS =~ ^[0-9]{1,4}$ ]] # should be 1-1000, no more than 4 digits needed
+    GUESSES=$(( $GUESSES + 1 ))
+    # Create game here:
+    # "INSERT INTO games(user_id) "
+
+    echo "$USER_GUESS ? $SECRET_NUMBER : $GUESSES"
+
+    if [ $USER_GUESS -eq $SECRET_NUMBER ]
     then
-      # check against random number and tell user if higher or lower
-      echo "$USER_GUESS ? $SECRET_NUMBER"
-      # game ends at some point, uncomment below and delete this line:
-      # GAME_FINISHED=true
-    else # not valid input
-      echo That is not an integer, guess again:
+      echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+      GAME_FINISHED=true
+    else 
+      if [ $USER_GUESS -lt $SECRET_NUMBER ]
+      then
+      echo "It's higher than that, guess again:"
+      else 
+        if [ $USER_GUESS -gt $SECRET_NUMBER ]
+        then
+          echo "It's lower than that, guess again:"
+        else 
+          echo "Unexpected input, guess again:"
+        fi
+      fi
     fi
-  
+
+  done
+
+    #   if [[ $USER_GUESS =~ ^[0-9]{1,4}$ ]] # should be 1-1000, no more than 4 digits needed
+    #   then
+    #     if [[ $USER_GUESS < $SECRET_NUMBER ]]
+    #     then 
+    #       echo "It's lower than that, guess again:"
+    #     else if [[ $USER_GUESS > $SECRET_NUMBER ]]; then
+    #       echo "It's higher than that, guess again:"
+    #     else if [[ $USER_GUESS == $SECRET_NUMBER ]]; then
+    #       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    #       GAME_FINISHED=true
+    #     else 
+    #       echo "Unexpected input, guess again:"
+    #     fi 
+    #   else
+    #     echo "That is not an integer, guess again:"
+    #   fi
+    # # done
 fi
