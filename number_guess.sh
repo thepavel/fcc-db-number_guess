4320834bb0a9432893c7b6ec5c2eb325d2e0f3f5
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-14 06:38:03 +0000

    feat: insert game

--- number_guess.sh
+++ number_guess.sh
@@ -21,52 +21,58 @@ else
     echo "Welcome, $USERNAME! It looks like this is your first time here."
     # TODO: create new user with entered username
     INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-    if [[ $INSERT_USER_RESULT != "1|0" ]]
+    if [[ $INSERT_USER_RESULT =~ "INSERT 0 1$" ]]
     then 
       echo $INSERT_USER_RESULT
     fi
-
+    
     USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
-    echo $USER_ID
   else
-    echo "$USER_ID"
+    
     # get games data
     GAMES_RESULT=$($PSQL "SELECT COUNT(game_id) AS games_played, MIN(guesses) as best_game FROM games")
     
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
+  
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, 1) RETURNING game_id" )
+  GAME_ID=$( echo $INSERT_GAME_RESULT | sed -E 's/^([0-9]+).*$/\1/' )
 
   GAME_FINISHED=false
   GUESSES=0
   echo "Guess the secret number between 1 and 1000:"
-    
-  # Create game here:
-  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, 1)")
 
   while [[ "$GAME_FINISHED" != "true" ]] ; do
     read USER_GUESS
     GUESSES=$(( $GUESSES + 1 ))
-    
+    # updating existing game
+      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET guesses = guesses + 1 WHERE game_id = $GAME_ID" )
+
     echo "$USER_ID guessed $USER_GUESS. there have been $GUESSES guesses to reach $SECRET_NUMBER"
 
-    if [ $USER_GUESS -eq $SECRET_NUMBER ]
+    if [ "$USER_GUESS" -eq "$SECRET_NUMBER" ]
     then
       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
       GAME_FINISHED=true
       # todo update game as finished? may make stats meaningless without that.
-      exit
+      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET is_completed = true WHERE game_id = $GAME_ID" )
+      
     else 
-      if [ $USER_GUESS -lt $SECRET_NUMBER ]
+      
+      GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
+      if [ $GUESSED_NUMBER -lt $SECRET_NUMBER ]
       then
         echo "It's higher than that, guess again:"
       else 
-        if [ $USER_GUESS -gt $SECRET_NUMBER ]
+        if [ $GUESSED_NUMBER -gt $SECRET_NUMBER ]
         then
           echo "It's lower than that, guess again:"
         else 
           echo "That is not an integer, guess again:"
         fi
       fi
+
     fi
   done
 
