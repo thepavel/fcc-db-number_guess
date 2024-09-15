6a03a66ddd65f1dacf6fc318222613e54e34ca1e
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-15 06:02:28 +0000

    feat: adds user id parsing
    fix: remove additional output
    
    feat: resolve game update issues by not updating a game
    
    feat: remove is_completed from queries
    
    feat: delete column and add data
    
    fix: remove test data
    
    feat: move user creation to game completion
    
    kRevert "feat: move user creation to game completion"
    
    This reverts commit 1e01b68e69e4f4746a6349ccaa53b17fc71ef510.

--- number_guess.sh
+++ number_guess.sh
@@ -5,62 +5,46 @@ MIN=1
 MAX=1000
 SECRET_NUMBER=$(($RANDOM%($MAX-$MIN+1)+$MIN))
 
-echo -e "\nEnter your username:"
+echo Enter your username:
 read USERNAME
 
 if [[ -z $USERNAME ]]
 then
-  echo Username required. Please try again.
+  exit
 else 
   # TODO validate input
-  # USER_ID=$($PSQL "SELECT users.user_id, COUNT(games.game_id) AS games_played, MIN(guesses) AS best_game FROM users LEFT JOIN games USING(user_id) WHERE username = '$USERNAME' GROUP BY users.user_id")
-  USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
   
   if [[ -z $USER_ID ]]
   then 
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-    # TODO: create new user with entered username
-    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-    if [[ $INSERT_USER_RESULT =~ "INSERT 0 1$" ]]
-    then 
-      echo $INSERT_USER_RESULT
-    fi
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') RETURNING user_id" )
     
-    USER_ID=$($PSQL "SELECT users.user_id FROM users WHERE username = '$USERNAME'")
-  else
-    
-    # get games data
-    GAMES_RESULT=$($PSQL "SELECT COUNT(game_id) AS games_played, MIN(guesses) as best_game FROM games")
+    USER_ID=$( echo $INSERT_USER_RESULT | sed -E 's/^([0-9]+).*$/\1/' )
+  else    
+
+    GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
+    BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $USER_ID")
     
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
   
-  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, 1) RETURNING game_id" )
-  GAME_ID=$( echo $INSERT_GAME_RESULT | sed -E 's/^([0-9]+).*$/\1/' )
-
   GAME_FINISHED=false
   GUESSES=0
-  echo "Guess the secret number between 1 and 1000:"
-
+  
   while [[ "$GAME_FINISHED" != "true" ]] ; do
+    echo "Guess the secret number between 1 and 1000:"
     read USER_GUESS
     GUESSES=$(( $GUESSES + 1 ))
-    # updating existing game
-      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET guesses = guesses + 1 WHERE game_id = $GAME_ID" )
+    GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
 
-    echo "$USER_ID guessed $USER_GUESS. there have been $GUESSES guesses to reach $SECRET_NUMBER"
-
-    if [ "$USER_GUESS" -eq "$SECRET_NUMBER" ]
+    if [ $GUESSED_NUMBER -eq $SECRET_NUMBER ]
     then
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESSES) RETURNING game_id" )
       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-
-      GAME_FINISHED=true
-      # todo update game as finished? may make stats meaningless without that.
-      UPDATE_GAME_RESULT=$( $PSQL "UPDATE games SET is_completed = true WHERE game_id = $GAME_ID" )
-      
+      exit
     else 
       
-      GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
       if [ $GUESSED_NUMBER -lt $SECRET_NUMBER ]
       then
         echo "It's higher than that, guess again:"
