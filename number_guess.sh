011ce439ce26807045bead9b53f6698155814662
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-17 06:22:25 +0000

    feat: WIP bug with inserting data on pulling existing user

--- number_guess.sh
+++ number_guess.sh
@@ -18,29 +18,39 @@ else
   if [[ -z $USER_ID ]]
   then 
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') RETURNING user_id" )
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username, games_played) VALUES('$USERNAME', 1) RETURNING user_id" )
     
     USER_ID=$( echo $INSERT_USER_RESULT | sed -E 's/^([0-9]+).*$/\1/' )
-  else    
-
-    GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
-    BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $USER_ID")
+  else   
+    # update games played
+    UPDATE_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_id = $USER_ID")
     
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id = $USER_ID")
+    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE user_id = $USER_ID")
+    USER_INFO=($PSQL "SELECT games_played, best_game FROM users WHERE user_id = $USER_ID")
+    echo "Welcome back, ${USERNAME}! You have played ${USER_INFO[0]} games, and your best game took ${USER_INFO[1]} guesses."
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
   
   GAME_FINISHED=false
   GUESSES=0
-  
+  echo "Guess the secret number between 1 and 1000:"
+
   while [[ "$GAME_FINISHED" != "true" ]] ; do
-    echo "Guess the secret number between 1 and 1000:"
+    
     read USER_GUESS
     GUESSES=$(( $GUESSES + 1 ))
     GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
 
     if [ $GUESSED_NUMBER -eq $SECRET_NUMBER ]
     then
-      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESSES) RETURNING game_id" )
+      echo $GUESSES
+      echo $BEST_GAME
+      # if [ $GUESSES -lt $BEST_GAME ] 
+      # then 
+      #   $( $PSQL "UPDATE users SET best_game = $BEST_GAME WHERE user_id = $USER_ID" )
+      # fi
+      
       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
       exit
     else 
@@ -59,22 +69,4 @@ else
 
     fi
   done
-
-    #   if [[ $USER_GUESS =~ ^[0-9]{1,4}$ ]] # should be 1-1000, no more than 4 digits needed
-    #   then
-    #     if [[ $USER_GUESS < $SECRET_NUMBER ]]
-    #     then 
-    #       echo "It's lower than that, guess again:"
-    #     else if [[ $USER_GUESS > $SECRET_NUMBER ]]; then
-    #       echo "It's higher than that, guess again:"
-    #     else if [[ $USER_GUESS == $SECRET_NUMBER ]]; then
-    #       echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-    #       GAME_FINISHED=true
-    #     else 
-    #       echo "Unexpected input, guess again:"
-    #     fi 
-    #   else
-    #     echo "That is not an integer, guess again:"
-    #   fi
-    # # done
 fi
