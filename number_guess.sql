f6be6d98a3f1a74881cce11b44ea83f7686239ae
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-18 05:42:42 +0000

    feat: makes best_game and games_played not nullable

--- number_guess.sql
+++ number_guess.sql
@@ -50,8 +50,8 @@ SET default_table_access_method = heap;
 CREATE TABLE public.users (
     user_id integer NOT NULL,
     username character varying(22) NOT NULL,
-    best_game integer,
-    games_played integer
+    best_game integer DEFAULT 0 NOT NULL,
+    games_played integer DEFAULT 0 NOT NULL
 );
 
 
