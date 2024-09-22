16d25e0b0e65fd67f8556cf918ef804d235b25a9
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-22 07:04:50 +0000

    fix: guess parsing was affecting score

--- number_guess.sh
+++ number_guess.sh
@@ -5,8 +5,6 @@ MIN=1
 MAX=1000
 SECRET_NUMBER=$(($RANDOM%($MAX-$MIN+1)+$MIN))
 
-NEW_USER=false
-
 echo Enter your username:
 read USERNAME
 
@@ -14,64 +12,52 @@ if [[ -z $USERNAME ]]
 then
   exit
 else 
-  # SELECT user_id, name, games_played, best_game FROM users WHERE name = 'testuser'
   USER_INFO=$($PSQL "SELECT user_id, name, games_played, best_game FROM users WHERE name = '$USERNAME'")
-  USER_INFO_ARRAY=( $( echo $USER_INFO | sed -E 's/\|/\ /g' ) )
+  GAME_FINISHED=false
+  GUESSES=0
+  
   # TODO validate input
-
   if [[ -z $USER_INFO ]]
-  then 
-    echo "Welcome, $USERNAME! It looks like this is your first time here."
-    NEW_USER=true
+  then
     INSERT_USER_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
-
+    
     USER_INFO=$($PSQL "SELECT user_id, name, games_played, best_game FROM users WHERE name = '$USERNAME'")
     USER_INFO_ARRAY=( $( echo $USER_INFO | sed -E 's/\|/\ /g' ) )
+
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
   else   
+    USER_INFO_ARRAY=( $( echo $USER_INFO | sed -E 's/\|/\ /g' ) )
     echo "Welcome back, $USERNAME! You have played ${USER_INFO_ARRAY[2]} games, and your best game took ${USER_INFO_ARRAY[3]} guesses."
   fi
 
   USER_ID=${USER_INFO_ARRAY[0]}
-  GAMES_PLAYED=${USER_INFO_ARRAY[2]}
   BEST_GAME=${USER_INFO_ARRAY[3]}
-  
-  GAME_FINISHED=false
-  GUESSES=0
+
   echo "Guess the secret number between 1 and 1000:"
 
   while [[ "$GAME_FINISHED" != "true" ]] ; do
     read USER_GUESS
-    GUESSED_NUMBER=$( echo $USER_GUESS | sed 's/[^0-9]*//g')
     
-    if [[ -z $GUESSED_NUMBER ]]; then 
+    if ! [[ $USER_GUESS =~ ^[0-9]+$ ]]
+    then
       echo "That is not an integer, guess again:"
       continue
-    fi
-echo 'it no worky'
+    else
+      ((++GUESSES))
 
-    echo "incrementing $GUESSES when guessing: $GUESSED_NUMBER"
-    GUESSES=$(( $GUESSES + 1 ))
-    if [ $GUESSED_NUMBER -eq $SECRET_NUMBER ]
-    then
-      
-      BEST_GAME=$(($GUESSES < $BEST_GAME || $BEST_GAME == 0 ? $GUESSES : $BEST_GAME))
-      
-      # update user
-      UPDATE_USER_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = $BEST_GAME WHERE user_id = $USER_ID")
-      
-      echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-      GAME_FINISHED=true
-    else 
-      if [ $GUESSED_NUMBER -lt $SECRET_NUMBER ]
-      then
+      if [ $USER_GUESS -lt $SECRET_NUMBER ] ; then
         echo "It's higher than that, guess again:"
-      else 
-        if [ $GUESSED_NUMBER -gt $SECRET_NUMBER ]
-        then
+      else if [ $USER_GUESS -gt $SECRET_NUMBER ] ; then
           echo "It's lower than that, guess again:"
+      else if [ $USER_GUESS -eq $SECRET_NUMBER ] ; then
+            GAME_FINISHED=true
+            BEST_SCORE=$(($BEST_GAME == 0 || $GUESSES < $BEST_GAME ? $GUESSES : $BEST_GAME))
+            UPDATE_USER_RESULT=$($PSQL "UPDATE users SET best_game = $BEST_GAME WHERE user_id = $USER_ID")
+            
+            echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"      
+          fi 
         fi
       fi
-
     fi
   done
 fi
